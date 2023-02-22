1054c8410fd8cf2a2978af5d28f6e19d5f765c88
Polina Sokolova <polina2001sokolovas@gmail.com>
2023-02-22 14:24:29 +0100

    2

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,15 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+#puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+Restaurant.create!(name: 'Dishoom', address: '7 Boundary St, London E2 7JE', phone_number: '34664346435', category:'french')
+Restaurant.create!(name: 'Pizza East', address: '56A Shoreditch High St, London E1 6PQ', phone_number: '34662246435', category: 'japanese')
+Restaurant.create!(name: 'La Papa', address: '3 Boundary St, London E2 7JE', phone_number: '34578546435', category:'chinese')
+Restaurant.create!(name: 'Chocolates', address: '7 Boundary St, Barcelona E2 7JE', phone_number: '34693346035', category:'french')
+Restaurant.create!(name: 'Arepas', address: '5 Boundary St, Moscow E2 7JE', phone_number: '3469368935', category:'belgian')
+
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
